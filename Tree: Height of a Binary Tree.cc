1ecde3e39851aeae39a83f503071bb5521d3d3d2
JiSooMoon <36117303+JiSooMoon@users.noreply.github.com>
2018-12-06 03:15:24 -0500

    Create Tree: Height of a Binary Tree.cc

---
+++ Tree: Height of a Binary Tree.cc
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root) {
+        // Write your code here.
+        int count = -1;
+        while(root != NULL){
+
+            if(root->right != NULL){
+                root = root->right;
+            }else
+                root = root->left;
+
+            
+            
+            
+
+            count++;
+        }
+        return count;
+    }
+
+}; //End of Solution
